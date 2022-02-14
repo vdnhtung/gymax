f6f5c6d86aa3cb0520fe5c14e66e91cf8ff4f865
vdnhtung <business.vudinhtung@gmail.com>
2022-02-14 10:52:16 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.nav-icon').click(function (e) { 
+        e.preventDefault();
+        $('.navbar').addClass('active');
+        $('.cover').addClass('active');
+    });
+    $('.cover').click(function (e) { 
+        e.preventDefault();
+        $('.navbar').removeClass('active');
+        $('.cover').removeClass('active');
+    });
+});
